d9e301090892c0f8e743867bc01f5413c4bbff8a
Philippe Aucazou <paucazou@yahoo.fr>
2017-10-30 19:01:29 +0100

    italic is now available

--- parsers/b_parser.py
+++ parsers/b_parser.py
@@ -4,19 +4,27 @@
 """This module manages the "b" letter"""
 
 
-def main(late_part,text,result):
+def main(**kw):
     """Send data to the right function"""
-    return functions[late_part[0]](late_part[1:],text,result)
+    return functions[late_part[0]](late_part=kw['late_part'][1:],text=kw['text'],result=kw['result'])
 
-def italic_parser():
+def italic_parser(**kw):
     """Return italic mark in tex"""
+    italic_part, mark, late_part = _endmark(kw['late_part'])
+    return "\textit{" + italic_part + '}' + late_part
+
+def underlined_parser(late_part,text,result):
     pass
 
-def underlined_parser():
+def bold_parser(late_part,text,result):
     pass
 
-def bold_parser():
-    pass
+def _endmark(line): # TODO fonction à mettre là où c'est nécessaire
+    """Verifies if an endmark exists at the right place
+    if True, return line.partition(',;')"""
+    if ',;' in line.partition(',; '):# TODO ce n'est pas sûr du tout.
+        raise SyntaxError("Marks are not correctly positioned in line {}".format(line)) # TODO add line number
+    return line.partition(',;')
 
 functions = {
         "i":italic_parser,
