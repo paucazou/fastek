9118f09ea4957a535b241bf0b851b7ea88c7c363
Philippe Aucazou <paucazou@yahoo.fr>
2017-10-30 19:01:42 +0100

    Added _load_file

--- main_parser.py
+++ main_parser.py
@@ -5,27 +5,28 @@
 
 import importlib
 import os
+import phlog
 import sys
 
 parsers_path = os.path.dirname(os.path.abspath(__file__)) + "/parsers/"
 sys.path.append(parsers_path)
 
+logger = phlog.loggers['console']
 mark = ",;"
+shabang = '#!/usr/bin/fastek' # TODO shaband must contain the type of document in arg : #!/usr/bin/fastek lesson
+suffix = ".ftk"
 parsers = {file[0]:importlib.import_module(file[:-3]) for file in os.listdir(parsers_path) if file[1:] == "_parser.py" in file
     } # a dict with a simple letter in key, and the module matching with it
 
-def main(file,result="latex"):
-    """Main parsers. file is a path with .ftk suffix TODO verify this
+def main(text,result="latex"): # TODO detect and make a warning for genuine latex marks
+    """Main parsers. text is a string
     result : type of text returned :
     latex (default): translate fastek to latex
-    raw : return raw text only"""
-    with open(file) as f:
-        text = f.load().split('\n')
+    raw : return raw text only"""    
     ntext = []
-
-    for line in text:
+    for i,line in enumerate(text):
         mark_count = line.count(mark)
-        for i in range(mark_count):
+        for j in range(mark_count):
             first_part, mark, late_part = line.partition(',;')
             if not late_part:
                 break
@@ -34,5 +35,18 @@ def main(file,result="latex"):
         ntext.append(line) # TODO traitement de la fin de ligne
     
     return '\n'.join(ntext)
+
+def _load_file(path):
+    """Loads an .ftk file and return it as a list of lines"""
+    if path[:-4] != suffix:
+        logger.warning("""{} has no {} suffix""".format(path,suffix))
+    with open(file) as f:
+        text = f.load().split('\n')
+    
+    if text[0].partition(' ')[0] != shabang:
+        raise ValueError("{} is not a valid fastek file".format(path))
+    return text
+        
+    
 
 
