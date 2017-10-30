c2b3e5bedfef3b7c2212def859b0aca86aed1da0
Philippe Aucazou <paucazou@yahoo.fr>
2017-10-30 19:01:07 +0100

    Added phlog

---
+++ phlog.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*
+#Deus, in adjutorium meum intende
+#This module is made to ease logging
+# Three modes : console or file or console + file
+
+import logging
+import logging.handlers
+
+formatter = logging.Formatter('%(filename)s :: %(funcName)s :: %(lineno)d :: %(levelname)s :: %(message)s') # essayer de rajouter la ligne, la fonction si possible
+levels = (logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG)
+
+def consoleLog(level,logger=logging.getLogger()):
+    """Console only.
+    Return logger.
+    level is an int which must equal to logging levels."""
+    if level not in levels:
+        raise ValueError("Unknown level. Level must be CRITICAL : {}, ERROR : {}, WARNING : {}, INFO : {}, DEBUG : {}".format(*levels))
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+def fileLog(level,file_name='activity.log',logger=logging.getLogger()):
+    """File only.
+    Return logger.
+    level is an int which must equal to logging levels."""
+    if level not in levels:
+        raise ValueError("Unknown level. Level must be CRITICAL : {}, ERROR : {}, WARNING : {}, INFO : {}, DEBUG : {}".format(*levels))
+    file_handler = logging.handlers.RotatingFileHandler(file_name,'a',1000000,1)
+    file_handler.setLevel(level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+def allLog(level,file_name='activity.log',logger=logging.getLogger()):
+    """File and console.
+    Return logger.
+    level is an int which must equal to logging levels."""
+    logger = consoleLog(level,logger)
+    logger = fileLog(level,file_name,logger)
+    return logger
+
+# convenient prepared loggers
+loggers = {}
+loggers['console'] = consoleLog(levels[-1],logging.getLogger('console'))
+#loggers['file'] = fileLog(levels[-1],logger=logging.getLogger('file')) # This creates an empty file, even if not called...
+#loggers['all'] = allLog(levels[-1],logger=logging.getLogger('all'))
+
+for value in loggers.values():
+    value.setLevel(levels[-1])
+
